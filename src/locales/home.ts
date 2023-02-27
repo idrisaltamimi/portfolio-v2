4206e2e246cc7d7502cf345af857153f7240f8cf
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-02-27 11:41:42 +0400

    commit

--- src/locales/home.ts
+++ src/locales/home.ts
@@ -1,11 +1,10 @@
 export const homeAr = {
-  overline: 'حان الوقت',
+  overline: 'أنا إدريس التميمي،',
   title: 'مطور مواقع',
   subtitle: 'أطور وأصنع مواقع وتطبيقات للمواقع',
   projects: 'مشاريعي',
   hireMe: 'احجز موعد',
   contact: 'تواصل معي',
-  about: 'لوريم ايبسوم دولار سيت أميت ,كونسيكتيتور أدايبا يسكينج أليايت,سيت دو أيوسمود تيمبور أنكايديديونتيوت لابوري ات دولار ماجنا أليكيوا . يوت انيم أد مينيم فينايم,كيواس نوستريد أكسير سيتاشن يللأمكو لابورأس نيسي يت أليكيوب أكس أيا كوممودو كونسيكيوات . ديواس أيوتي أريري دولار إن ريبريهينديرأيت فوليوبتاتي فيلايت أيسسي كايلليوم دولار أيو فيجايت نيولا باراياتيور. أيكسسيبتيور ساينت أوككايكات كيوبايداتات نون بروايدينت ,سيونت ان كيولبا كيو أوفيسيا ديسيريونتموليت انيم أيدي ايست لابوريوم.'
 }
 export const homeEn = {
   overline: 'I am Idris Al Tamimi,',
@@ -14,5 +13,4 @@ export const homeEn = {
   projects: 'My Projects',
   contact: 'Contact Me',
   hireMe: 'Schedule Meeting',
-  about: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. '
 }
